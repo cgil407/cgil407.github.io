103166e78ad1d36a6168433dda09fc3a43aa266b
cgil407 <cgil407@wgu.edu>
2025-07-18 13:38:17 -0400

    removed everything from D288 project and replaced with D279 project files so i can use pages hosting service

--- scripts/confirmEmail.js
+++
@@ -1,9 +0,0 @@
-function validateEmail() {
-    var email = document.getElementById("email").value;
-    var confirmEmail = document.getElementById("confirmEmail").value;
-
-    if (email !== confirmEmail) {
-        alert("Emails do not match, please try again.");
-        event.preventDefault();
-    }
-}
